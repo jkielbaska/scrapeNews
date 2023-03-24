3d00fd600066ef5f7a5d01d247e523856bd691a6
Jakub <kielbaskakuba@gmail.com>
2023-03-25 00:27:55 +0100

    Add neccesary layout wrapper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/global.css";
+import { Footer } from "@/components/organisms/Footer";
+import { Header } from "@/components/organisms/Header";
+import { Layout } from "@/components/molecules/Layout";
+import { AppPropsWithLayout } from "../types/tNextPageLayout";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  return (
+    <Layout>
+      <Component {...pageProps} />;
+    </Layout>
+  );
 }
