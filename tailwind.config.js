796228078fd1a30158f1bc4b3942cb140f35b12e
Jakub <kielbaskakuba@gmail.com>
2023-03-25 00:17:36 +0100

    Add basic theme extends for better usability

---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  important: "#__next",
+  theme: {
+    extend: {
+      height: {
+        "10v": "10vh",
+        "20v": "20vh",
+        "25v": "25vh",
+        "30v": "30vh",
+        "40v": "40vh",
+        "50v": "50vh",
+        "60v": "60vh",
+        "70v": "70vh",
+        "80v": "80vh",
+        "90v": "90vh",
+        "100v": "100vh",
+        90: "22.5rem",
+      },
+      width: {
+        "10v": "10vw",
+        "20v": "20vw",
+        "30v": "30vw",
+        "40v": "40vw",
+        "50v": "50vw",
+        "60v": "60vw",
+        "70v": "70vw",
+        "80v": "80vw",
+        "90v": "90vw",
+        "100v": "100vw",
+      },
+      maxWidth: {
+        "10r": "10rem",
+      },
+      top: {
+        header: "var(--header-height)",
+      },
+      fontSize: {
+        title: "clamp(1rem, 7.2vw, 4rem)",
+        subtitle: "clamp(0.7rem, 5.9vw, 3rem)",
+      },
+    },
+  },
+  plugins: [],
+};
