5d2f89d50eafad5bb2acba5c6c179df5444aff56
Jakub <kielbaskakuba@gmail.com>
2023-03-28 23:54:58 +0200

    Comment Footer in Layout. Have to improve how it is displayed.

--- src/components/molecules/Layout.tsx
+++ src/components/molecules/Layout.tsx
@@ -3,8 +3,16 @@ import { ReactNode } from "react";
 import { Footer } from "../organisms/Footer";
 import { Header } from "../organisms/Header";
 import { SideMenu } from "../organisms/SideMenu";
+// import { useDispatch, useSelector } from "react-redux";
+// import { setLayout } from "@/redux/store";  //docelowo layout w layoucie
 
-export const Layout = ({ children }: { children: ReactNode }) => {
+export const Layout = ({
+  children,
+  totalResults,
+}: {
+  children: ReactNode;
+  totalResults?: number;
+}) => {
   return (
     <>
       <div className="flex flex-col min-h-screen">
@@ -13,12 +21,13 @@ export const Layout = ({ children }: { children: ReactNode }) => {
           {/* <div className=" lg:block lg:w-2/12 xl:w-1/5">
             <SideMenu />
           </div> */}
-          <main className="min-h-screen flex flex-col justify-start  pt-[120px]">
+          <main className="min-h-screen pt-[120px] flex flex-col">
             {children}
           </main>
         </div>
-        <Footer />
+        {/* <Footer totalResults={children?.props.countryNews.totalResults} /> */}
       </div>
     </>
   );
 };
+//children.props.countryNews.totalResults?? that should be here, not on every page https://stackoverflow.com/questions/60893121/how-to-properly-type-react-children-and-react-cloneelement
