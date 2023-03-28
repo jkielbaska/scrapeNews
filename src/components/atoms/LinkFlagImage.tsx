c1efaa14135a9e8cdf389b716e5b0803ec17e3c6
Jakub <kielbaskakuba@gmail.com>
2023-03-28 23:48:34 +0200

    Add component to display flags and countries. Next.js Link to country/[kraj] url, add package to display full names

---
+++ src/components/atoms/LinkFlagImage.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+import countryList from "country-list";
+import { Typography } from "@mui/material";
+
+export const LinkFlagImage = ({ country }: { country: string }) => {
+  const countryName = countryList.getName(country);
+  const flagUrl = `https://flagcdn.com/32x24/${country}.png`;
+  return (
+    <>
+      <Link href={`/country/${country}`}>
+        <div className="w-full h-full flex justify-center flex-col items-center">
+          <Image
+            alt={`flag of ${country}`}
+            src={flagUrl}
+            width={32}
+            height={24}
+          />
+          <Typography>{countryName}</Typography>
+        </div>
+      </Link>
+    </>
+  );
+};
