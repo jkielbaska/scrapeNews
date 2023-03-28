28b43c546cee841639d3bc3b606c210d43f8b2f0
Jakub <kielbaskakuba@gmail.com>
2023-03-29 00:05:50 +0200

    Add homepage. Support for view change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,32 @@
-import Head from "next/head";
-import Image from "next/image";
+import { useCountriesName } from "@/hooks/useCountriesName";
+import { LinkFlagImage } from "@/components/atoms/LinkFlagImage";
+import { useSelector } from "react-redux";
 
 export default function Home() {
+  const { countriesData } = useCountriesName();
+  const layout = useSelector((state: any) => state.layout.layout);
   return (
     <>
-      <div>
-        <p>aaa</p>
+      <h3 className="pt-[70px] flex justify-center text-gray-400 text-2xl">
+        See news from choosen country
+      </h3>
+      <div className="flex flex-wrap justify-center">
+        {countriesData.map((country) => {
+          return (
+            <div
+              key={country.id}
+              className={
+                layout === "block"
+                  ? "w-[200px] h-[200px] text-xl m-3 flex items-center justify-center  font-serif bg-gray-400 border-sky-700 border-2 rounded-md"
+                  : layout === "list"
+                  ? "m-2 w-full font-serif bg-gray-400 border-sky-700 border-2 rounded-md"
+                  : ""
+              }
+            >
+              <LinkFlagImage country={country.country} />
+            </div>
+          );
+        })}
       </div>
     </>
   );
