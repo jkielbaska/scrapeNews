1f55bba1e8ba391382843cf33789878571198cb0
Jakub <kielbaskakuba@gmail.com>
2023-03-28 23:55:25 +0200

    Clock throwing hydration error. Have to improve how it is displayed.

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,13 +1,22 @@
+import Clock from "react-live-clock";
+
 // aktualna godzina
 // liczba artykułów wyświetlanych w głównym contencie
 
-export const Footer = () => {
+export const Footer = ({
+  totalResults,
+}: {
+  totalResults?: number | undefined;
+}) => {
   //wyświetl aktualną datę po prawej stronie footera w formacie dd.mm.yyyy
 
   return (
-    <div>
-      <footer className="mt-auto bg-gray-900 text-white p-4 relative">
-        <p>footer</p>
+    <div className="w-100v">
+      <footer className=" mt-auto bg-gray-800 text-white p-4 relative flex flex-row justify-between">
+        {/* <Clock format="HH:mm:ss" interval={1000} ticking={true} /> */}
+        {totalResults !== undefined && (
+          <p> {totalResults} articles on the page</p>
+        )}
       </footer>
     </div>
   );
