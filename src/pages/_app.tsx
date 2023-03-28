7c032eaa6e035a3c25641fb43257f194d685e319
Jakub <kielbaskakuba@gmail.com>
2023-03-29 00:02:49 +0200

    Add Redux Provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,15 @@ import { Footer } from "@/components/organisms/Footer";
 import { Header } from "@/components/organisms/Header";
 import { Layout } from "@/components/molecules/Layout";
 import { AppPropsWithLayout } from "../types/tNextPageLayout";
+import { store } from "@/redux/store";
+import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   return (
-    <Layout>
-      <Component {...pageProps} />;
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
