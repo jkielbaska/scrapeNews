f1dccb77616d64e22b17e31048b699b0d42dff34
Jakub <kielbaskakuba@gmail.com>
2023-03-29 00:07:32 +0200

    Add viewChange support

--- src/pages/country/[kraj].tsx
+++ src/pages/country/[kraj].tsx
@@ -1,38 +1,56 @@
 import Head from "next/head";
-import Image from "next/image";
 import { ListView } from "@/components/molecules/ListView";
+import { BlockView } from "@/components/molecules/BlockView";
 import { Params, CountryNewsResponse } from "@/types/tNewsResponse";
-import { getCountryNews, getTopNews } from "@/services/api";
+import { getCountryNews } from "@/services/api";
+import { useSelector } from "react-redux";
 
 export async function getServerSideProps({ params }: { params: Params }) {
   const countryNews = await getCountryNews(params.kraj);
   return {
     props: {
       countryNews,
-      params: params.kraj,
+      country: params.kraj,
     },
   };
 }
 
 const CountryById = ({
   countryNews,
-  params,
+  country,
 }: {
   countryNews: CountryNewsResponse;
-  params: Params;
+  country: Params;
 }) => {
-  console.log(countryNews, params);
-  return (
-    <div className="flex flex-col items-center pt-5 ">
-      <Head>
-        <title>News - NewsAPI</title>
-      </Head>
+  console.log(countryNews, country);
+  const layout = useSelector((state: any) => state.layout.layout);
 
-      <div>
-        <ListView countryNews={countryNews}></ListView>
+  return (
+    <>
+      <div className="flex flex-wrap justify-center pt-5 text-center">
+        <Head>
+          <title>News - NewsAPI</title>
+        </Head>
+
+        <div>
+          {layout === "block" ? (
+            <BlockView countryNews={countryNews} />
+          ) : layout === "list" ? (
+            <ListView countryNews={countryNews} />
+          ) : (
+            <ListView countryNews={countryNews} />
+          )}
+        </div>
+        {/* <Footer totalResults={countryNews.totalResults} /> */}
       </div>
-    </div>
+    </>
   );
 };
 
 export default CountryById;
+{
+  /* <div className="flex flex-col items-center  pt-5 text-center"> */
+}
+{
+  /* <div className="flex flex-wrap justify-center"></div> */
+}
