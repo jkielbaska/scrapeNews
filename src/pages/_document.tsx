2c8e8b5ada608226cdc356375eaaa3103b55c9ac
Jakub <kielbaskakuba@gmail.com>
2023-03-29 00:03:29 +0200

    Add simple meta data in Head.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="News page, best on the internet" />
+        <meta name="keywords" content="news, best, internet" />
+        <meta name="author" content="J.Kielbaska" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
