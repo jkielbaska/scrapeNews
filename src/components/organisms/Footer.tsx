16341921528f616cdc4e45a02c188521a053ee5d
Jakub <kielbaskakuba@gmail.com>
2023-03-29 23:28:13 +0200

    Display hour using useTime hook. Handle hydration error

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,22 +1,23 @@
-import Clock from "react-live-clock";
-
-// aktualna godzina
-// liczba artykułów wyświetlanych w głównym contencie
+import { useTime } from "@/hooks/useTime";
 
 export const Footer = ({
   totalResults,
 }: {
   totalResults?: number | undefined;
 }) => {
-  //wyświetl aktualną datę po prawej stronie footera w formacie dd.mm.yyyy
+  const { currentHour, hydrated } = useTime();
+
+  if (!hydrated) {
+    return null;
+  }
 
   return (
-    <div className="w-100v">
+    <div className="w-100v fixed bottom-0">
       <footer className=" mt-auto bg-gray-800 text-white p-4 relative flex flex-row justify-between">
-        {/* <Clock format="HH:mm:ss" interval={1000} ticking={true} /> */}
         {totalResults !== undefined && (
           <p> {totalResults} articles on the page</p>
         )}
+        <p>{currentHour}</p>
       </footer>
     </div>
   );
