768b3a649d58995f1e16acf8fe427674a328daa5
Jakub <kielbaskakuba@gmail.com>
2023-03-29 23:31:30 +0200

    Delete unnecessary comments to improve code readability

--- src/components/organisms/Header/Header.test.tsx
+++ src/components/organisms/Header/Header.test.tsx
@@ -14,38 +14,3 @@ describe("Header component", () => {
     expect(header).toBeInTheDocument();
   });
 });
-
-// describe("ViewChanger component", () => {
-//   it("should dispatch setLayout action when a button is clicked", () => {
-//     const { getByTestId } = render(
-//       <Provider store={store}>
-//         <ViewChanger />
-//       </Provider>
-//     );
-
-//     fireEvent.click(getByTestId("blockbutton"));
-//     expect(store.getState().layout).toBe("block");
-
-//     fireEvent.click(getByTestId("list-utton"));
-//     expect(store.getState().layout).toBe("list");
-//   });
-// });
-
-// it("should dispatch setLayout action when ViewChanger button is clicked", () => {
-//   const { getByTestId } = render(
-//     <Provider store={store}>
-//       <Header />
-//     </Provider>
-//   );
-
-//   const blockButton = getByTestId("view-changer-block-button");
-//   const listButton = getByTestId("view-changer-list-button");
-
-//   // Check that clicking the block button dispatches setLayout with "block"
-//   blockButton.click();
-//   expect(store.getState().layout).toEqual("block");
-
-//   // Check that clicking the list button dispatches setLayout with "list"
-//   listButton.click();
-//   expect(store.getState().layout).toEqual("list");
-// });
