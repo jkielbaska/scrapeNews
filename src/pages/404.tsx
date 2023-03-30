45767124f1b9c44392dd462c6d731d8c74abaea5
Jakub <kielbaskakuba@gmail.com>
2023-03-30 22:14:22 +0200

    Add custom 404 page to improve user experience

---
+++ src/pages/404.tsx
@@ -0,0 +1,7 @@
+export default function Custom404() {
+  return (
+    <div className="w-100v h-60v flex justify-center items-center text-3xl text-white ">
+      <h1>404 - Page Not Found</h1>
+    </div>
+  );
+}
