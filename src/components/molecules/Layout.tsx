a1ed6be22f751298030931a55c19885a251692b8
Jakub <kielbaskakuba@gmail.com>
2023-03-30 22:22:07 +0200

    Add IntlProvider to handle language change, use redux selecotor here

--- src/components/molecules/Layout.tsx
+++ src/components/molecules/Layout.tsx
@@ -1,15 +1,29 @@
+import { LOCALES } from "@/i18n/locales";
+import { messages } from "@/i18n/messages";
+import { RootState } from "@/redux/store";
 import { ReactNode } from "react";
+import { IntlProvider } from "react-intl";
+import { useSelector } from "react-redux";
 import { Header } from "../organisms/Header";
 
 export const Layout = ({ children }: { children: ReactNode }) => {
+  const locale = LOCALES.ENGLISH;
+  const lang = useSelector((state: RootState) => state.lang.lang);
+
   return (
     <>
-      <div className="flex flex-col">
-        <Header />
-        <div>
-          <main className="pt-[120px] flex flex-col">{children}</main>
+      <IntlProvider
+        messages={messages[lang]}
+        locale={locale}
+        defaultLocale={LOCALES.ENGLISH}
+      >
+        <div className="flex flex-col">
+          <Header />
+          <div>
+            <main className="pt-[120px] flex flex-col">{children}</main>
+          </div>
         </div>
-      </div>
+      </IntlProvider>
     </>
   );
 };
