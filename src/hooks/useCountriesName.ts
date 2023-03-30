2454e42b2ae1209a84bf50397ac645a88f9aec70
Jakub <kielbaskakuba@gmail.com>
2023-03-30 22:15:28 +0200

    Add undefined property to hook because of try/catch block

--- src/hooks/useCountriesName.ts
+++ src/hooks/useCountriesName.ts
@@ -3,21 +3,23 @@ import { getTopNews } from "@/services/api";
 import { Source } from "@/types/tNewsResponse";
 
 export const useCountriesName = () => {
-  const [countriesData, setCountriesData] = useState<Source[]>([]);
+  const [countriesData, setCountriesData] = useState<Source[] | undefined>([]);
 
   const fetchCountries = async () => {
     const response = await getTopNews();
-    const sources = response.sources;
+    const sources = response?.sources;
 
-    const updatedCountries = sources
-      .concat({
-        id: "plnd",
-        name: "unknown",
-        country: "pl",
-      })
-      .filter((item) => item.country !== "zh");
+    const updatedCountries =
+      sources &&
+      sources
+        .concat({
+          id: "plnd",
+          name: "unknown",
+          country: "pl",
+        })
+        .filter((item) => item.country !== "zh");
 
-    const uniqueCountries = updatedCountries.reduce(
+    const uniqueCountries = updatedCountries?.reduce(
       (acc: Source[], source: Source) => {
         const country = source.country;
         if (!acc.some((item) => item.country === country)) {
