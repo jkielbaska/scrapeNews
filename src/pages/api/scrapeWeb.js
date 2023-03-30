9649e7fb001e8c712f55dbf23949ba170484a7c4
Jakub <kielbaskakuba@gmail.com>
2023-03-30 22:13:59 +0200

    Add cosmetic changes, add isPending to handle state when fetching

--- src/pages/api/scrapeWeb.js
+++ src/pages/api/scrapeWeb.js
@@ -5,21 +5,26 @@ const { Readability } = require("@mozilla/readability");
 export default async function handler(req, res) {
   const { url } = req.query;
   const decodedUrl = decodeURIComponent(url);
+  let isPending = true;
 
   try {
+    isPending = true;
     const r1 = await axios.get(decodedUrl);
     const dom = new JSDOM(r1.data, {
       url: decodedUrl,
     });
     const article = new Readability(dom.window.document).parse();
-    res.status(200).send(article.content); // or .textContent
+    content = article.content;
+    res.status(200).send({ isPending, content }); // or .textContent
   } catch (error) {
     console.error(error);
     res
       .status(500)
       .send(
-        "Can't load this page due to security polisy. If you want to see whole article, click on 'Click to see original article'"
+        "Can't load this page due to security polisy. If you want to see whole article, click on 'Link to article'"
       );
     res.status(404).json({ error: "page not found" });
+  } finally {
+    isPending = false;
   }
 }
