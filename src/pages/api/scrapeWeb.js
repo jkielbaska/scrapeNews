ead37ea2e0100bfec54f41bce834ac84c3e06941
Jakub <kielbaskakuba@gmail.com>
2023-03-31 15:54:46 +0200

    Add headers and simple fix to handler function

--- src/pages/api/scrapeWeb.js
+++ src/pages/api/scrapeWeb.js
@@ -6,16 +6,25 @@ export default async function handler(req, res) {
   const { url } = req.query;
   const decodedUrl = decodeURIComponent(url);
   let isPending = true;
+  let content;
+
+  const session = axios.create({
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
+      "Content-Type": "text/html",
+    },
+  });
 
   try {
     isPending = true;
-    const r1 = await axios.get(decodedUrl);
+    const r1 = await session.get(decodedUrl);
     const dom = new JSDOM(r1.data, {
       url: decodedUrl,
     });
     const article = new Readability(dom.window.document).parse();
-    content = article.content;
-    res.status(200).send({ isPending, content }); // or .textContent
+    content = article?.content;
+    res.status(200).send(content); // or .textContent
   } catch (error) {
     console.error(error);
     res
@@ -23,7 +32,6 @@ export default async function handler(req, res) {
       .send(
         "Can't load this page due to security polisy. If you want to see whole article, click on 'Link to article'"
       );
-    res.status(404).json({ error: "page not found" });
   } finally {
     isPending = false;
   }
