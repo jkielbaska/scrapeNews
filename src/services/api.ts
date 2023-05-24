157ee4c8ded29f52f89f69aa019d4a33d37f8180
Jakub <kielbaskakuba@gmail.com>
2023-05-24 14:31:01 +0200

    Delete key from history, change to env.API_KEY

---
+++ src/services/api.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { TopNewsResponse, CountryNewsResponse } from "@/types/tNewsResponse";
+
+const apiBase = "https://newsapi.org/v2";
+const API_KEY = process.env.API_KEY;
+
+export const api = axios.create({
+  baseURL: apiBase,
+  params: {
+    apiKey: API_KEY,
+  },
+});
+
+export const getTopNews = async () => {
+  try {
+    const response = await api.get<TopNewsResponse>("/top-headlines/sources?");
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getCountryNews = async (
+  country: string | undefined | string[]
+) => {
+  try {
+    const response = await api.get<CountryNewsResponse>(
+      `/top-headlines?country=${country}`
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
