ad31440dacf43d97851f7a87344ca5a15acfc527
Jakub <kielbaskakuba@gmail.com>
2023-05-24 13:22:26 +0200

    Delete unnecessary code and add new header

--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { HeadPopup } from "../../atoms/HeadPopup";
 import { ViewChanger } from "../../molecules/ViewChanger";
 import { SideMenu } from "../../molecules/SideMenu";
 import { LangChanger } from "@/components/molecules/LangChanger";
@@ -9,13 +8,12 @@ export const Header = () => {
     <header className=" bg-gray-800 text-white p-4 w-100v fixed top-0">
       <div className="flex flex-row justify-between">
         <Link href="/">
-          <h1 className="text-3xl font-bold pb-3">GnNews</h1>
+          <h1 className="text-3xl font-bold pb-3">scrapeNews.</h1>
         </Link>
         <ViewChanger />
         <LangChanger />
       </div>
       <SideMenu />
-      <HeadPopup />
     </header>
   );
 };
